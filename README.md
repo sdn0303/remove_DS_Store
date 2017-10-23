6f74b3e7ced87f0d1d05715cbc1378f71dd5f8ed
David Soichi Nakahashi <sdn0303.guitar@gmail.com>
2017-10-23 11:11:19 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Remove .DS_Store
+* 作業ディレクトリ内にできる.DS_Storeを探して削除するスクリプト。
+```commandline
+$ python cmd.py
+```
---
+++ cmd.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
+
+
+def file_iterator(root_dir: str) -> str:
+    """ ディレクトリ巡回して
+    ファイルパス一覧を取得
+    """
+    for path, dirs, files in os.walk(root_dir):
+        yield path
+        for file in files:
+            yield os.path.join(path, file)
+
+
+if __name__ == "__main__":
+    """ ディレクトリ内に
+    .DS_Storeが存在したら削除する。
+    """
+    for file_path in file_iterator(root_dir='.'):
+        if ".DS_Store" in file_path:
+            os.remove(file_path)
+        else:
+            print("Directory is clean.")
